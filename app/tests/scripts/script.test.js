277a40e9dcba271e92541f420d6db1d46e685532
renke <renke@gmx.de>
2015-03-26 23:04:33 +0100

    add test for test

--- app/tests/scripts/script.test.js
+++ app/tests/scripts/script.test.js
@@ -4,13 +4,12 @@ describe('appendText', function(){
 
 	beforeEach( function(){
 		//$('<div class="wrapper"></div>').remove();
-		//$('<div class="wrapper">hier clicken</div>').appendTo('body');
-
-		
+		$('<div class="wrapper"/>').appendTo('body');
 	});
 
 	afterEach( function(){
 		//$('<div class="wrapper" />').remove();
+		$(".wrapper").remove();
 	})
 
 	it("should return null on init", function(){
@@ -23,26 +22,21 @@ describe('appendText', function(){
 		expect(html.remoteSwitch().cnt).toBe( 0 );
 	});
 
-	// it("counter should be increase step 1 on click", function(){
-	// 	var html = $(".wrapper");
-	// 	html.remoteSwitch();
-	// 	console.log("..", html.remoteSwitch().cnt);
-	// 	html.click();
-	// 	console.log("..", html.remoteSwitch().cnt);
-	// 	expect(html.remoteSwitch().cnt).toBe( 1 );
-	// });
+	it("counter should be increase step 1 on click", function(){
+		var html = $(".wrapper");
+		html.remoteSwitch();		
+		$(html).click();		
+		expect(html.remoteSwitch().cnt).toBe( 1 );
+	});
 	
+	it("should append text after click", function(){
+		var html = $(".wrapper");
+		html.remoteSwitch();
+		expect( $(html).html() ).toBe('');
 
-	// it("should append text after click", function(){
-	// 	var html = $(".wrapper");
-
-	// 	$(html).append('hier clicken');
-	// 	// html.remoteSwitch();
-	// 	// expect( $(html).html() ).toBe(undefined);
-
-	// 	$(html).click();
-	// 	expect( $(html).html() ).toBe(undefined);
-	// });
+		$(html).click();
+		expect( $(html).html() ).toBe('renke');
+	});
 
 	
 
